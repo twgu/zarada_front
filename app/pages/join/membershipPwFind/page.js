346ddc45d4547c8a6fee22c9d2e9c2d2f2c3fee2
구태완 <gutw@donga.co.kr>
2023-12-14 11:06:49 +0900

    비밀번호 찾기 기능구현

--- app/pages/join/membershipPwFind/page.js
+++ app/pages/join/membershipPwFind/page.js
@@ -1,3 +1,172 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import MembershipHeader from "@/app/components/MembershipHeader";
+
 export default function MembershipPwFind() {
-  return <div>MembershipPwFind 페이지</div>;
+  const router = useRouter();
+
+  const [idError, setIdError] = useState(false);
+  const [idVal, setIdVal] = useState("");
+  const [phoneVal, setPhoneVal] = useState("");
+  const [nameVal, setNameVal] = useState("");
+
+  const fPwFind = () => {
+    if (idVal == "") {
+      alert("아이디를 입력해 주시기 바랍니다.");
+      return;
+    }
+
+    if (phoneVal == "" || nameVal == "") {
+      alert("휴대폰인증 후 진행하시기 바랍니다.");
+      return;
+    }
+
+    axios
+      .post(process.env.NEXT_PUBLIC_API_URL + "/userIdcheck", {
+        findPwId: idVal,
+        userPhone: phoneVal,
+        userName: nameVal,
+      })
+      .then((response) => {
+        if (response.data.idcount > 0) {
+          localStorage.setItem("findUserCo", response.data.userCo);
+          router.push("/pages/join/membershipPwFind/success", {
+            scroll: false,
+          });
+        } else {
+          alert("등록된 회원정보가 없습니다. 입력정보를 다시 확인해주세요.");
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const onChangeHandler = (e, id) => {
+    switch (id) {
+      case "id":
+        if (e.target.value == "") {
+          setIdError(true);
+        } else {
+          setIdError(false);
+        }
+        setIdVal(e.target.value);
+        break;
+    }
+  };
+
+  const onBlurHandler = (e, id) => {
+    switch (id) {
+      case "id":
+        if (idVal == "") {
+          setIdError(true);
+        }
+        break;
+    }
+  };
+
+  const onClickHandler = (id) => {
+    switch (id) {
+      case "nicePhone":
+        // TODO : NICE 인증
+        alert("나이스 휴대폰 인증 모듈 호출");
+        setPhoneVal("01023089903");
+        setNameVal("구태완");
+        break;
+      case "pwFind":
+        fPwFind();
+        break;
+    }
+  };
+
+  return (
+    <div className="h-project-template">
+      <MembershipHeader title="비밀번호 찾기" />
+      <div className="h-project-content join-membership">
+        <h3 className="join-tit">아이디 입력 후 휴대폰 인증을 진행하세요</h3>
+        <div className="tab-template">
+          <div className={idError ? "form-group error" : "form-group valid"}>
+            <div className="input-area">
+              <input
+                type="text"
+                id="find-pw-id"
+                placeholder="아이디"
+                className="default-input"
+                value={idVal}
+                onChange={(e) => {
+                  onChangeHandler(e, "id");
+                }}
+                onBlur={(e) => {
+                  onBlurHandler(e, "id");
+                }}
+              />
+              <label htmlFor="find-pw-id" className="blind">
+                아이디
+              </label>
+            </div>
+            {idError ? (
+              <div className="form-error is-visible">
+                <div data-validation-attr="아이디을 입력해주세요">
+                  아이디을 입력해주세요
+                </div>
+              </div>
+            ) : null}
+          </div>
+          <div className="form-group">
+            <div className="input-area calc-input-area">
+              <input
+                type="num"
+                className="default-input"
+                id="agree-phone-number"
+                placeholder="휴대폰 번호"
+                value={phoneVal}
+                disabled
+              />
+              <label htmlFor="agree-phone-number" className="blind">
+                휴대폰 번호
+              </label>
+              <button
+                type="button"
+                className="agree-right-btn"
+                onClick={() => {
+                  onClickHandler("nicePhone");
+                }}
+              >
+                인증
+              </button>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="input-area">
+              <input
+                type="text"
+                className="default-input"
+                id="find-pw-name"
+                placeholder="보호자 이름"
+                value={nameVal}
+                disabled
+              />
+              <label htmlFor="find-pw-name" className="blind">
+                보호자 이름
+              </label>
+            </div>
+          </div>
+          <div className="bottom-fixed">
+            <button
+              type="button"
+              className="default-block-btn"
+              onClick={() => {
+                onClickHandler("pwFind");
+              }}
+            >
+              비밀번호 찾기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/pages/join/membershipPwFind/success/page.js
+++ app/pages/join/membershipPwFind/success/page.js
@@ -1,3 +1,183 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import MembershipHeader from "@/app/components/MembershipHeader";
+
+const pwReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,16}/; //6-16자리 영문, 숫자, 특수문자 조합
+
 export default function Success() {
-  return <div>Success 페이지</div>;
+  const router = useRouter();
+
+  const [pwError, setPwError] = useState(false);
+  const [pwErrorMsg, setPwErrorMsg] = useState("");
+  const [pwVal, setPwVal] = useState("");
+  const [pwOkError, setPwOkError] = useState(false);
+  const [pwOkErrorMsg, setPwOkErrorMsg] = useState("");
+  const [pwOkVal, setPwOkVal] = useState("");
+
+  const fPwChange = () => {
+    if (pwError || pwOkError) {
+      alert("비밀번호와 비밀번호 재설정 필드 모두 올바른 값을 입력 해주세요.");
+      return;
+    }
+    if (!pwReg.test(pwOkVal)) {
+      alert(
+        "비밀번호는 6-16자의 영문, 숫자, 특수문자 조합으로 입력해야합니다."
+      );
+      return;
+    }
+
+    axios
+      .post(process.env.NEXT_PUBLIC_API_URL + "/updatePwd", {
+        user_co: localStorage.getItem("findUserCo"),
+        newpwd: pwOkVal,
+      })
+      .then((response) => {
+        alert("비밀번호 변경이완료되었습니다.");
+        router.push("/pages/login", {
+          scroll: false,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const onChangeHandler = (e, id) => {
+    switch (id) {
+      case "pw":
+        if (e.target.value == "") {
+          setPwError(true);
+          setPwErrorMsg("비밀번호 재설정 필드를 입력해주세요.");
+        } else {
+          if (e.target.value.length < 6) {
+            setPwError(true);
+            setPwErrorMsg("최소 6자 이상 입력해주세요.");
+          } else {
+            setPwError(false);
+          }
+        }
+        if (pwOkVal.length > 0) {
+          if (e.target.value != pwOkVal) {
+            setPwOkError(true);
+            setPwOkErrorMsg("비밀번호가 일치하지 않습니다.");
+          } else {
+            setPwOkError(false);
+          }
+        }
+        setPwVal(e.target.value);
+        break;
+      case "pwOk":
+        if (e.target.value == "") {
+          setPwOkError(true);
+          setPwOkErrorMsg("비밀번호 재설정 확인 필드를 입력해주세요.");
+        } else {
+          if (e.target.value != pwVal) {
+            setPwOkError(true);
+            setPwOkErrorMsg("비밀번호가 일치하지 않습니다.");
+          } else {
+            setPwOkError(false);
+          }
+        }
+        setPwOkVal(e.target.value);
+        break;
+    }
+  };
+
+  const onBlurHandler = (e, id) => {
+    switch (id) {
+      case "pw":
+        if (pwVal == "") {
+          setPwError(true);
+          setPwErrorMsg("비밀번호 재설정 필드를 입력해주세요.");
+        }
+        break;
+      case "pwOk":
+        if (pwOkVal == "") {
+          setPwOkError(true);
+          setPwOkErrorMsg("비밀번호 재설정 확인 필드를 입력해주세요.");
+        }
+        break;
+    }
+  };
+
+  const onClickHandler = (id) => {
+    switch (id) {
+      case "pwChange":
+        fPwChange();
+        break;
+    }
+  };
+
+  return (
+    <div className="h-project-template">
+      <MembershipHeader title="비밀번호 찾기" />
+      <div className="h-project-content join-membership">
+        <h3 className="join-tit">비밀번호를 재설정 해주세요</h3>
+        <div className="tab-template">
+          <div className={pwError ? "form-group error" : "form-group valid"}>
+            <div className="input-area">
+              <label className="blind" htmlFor="pw-reset">
+                비밀번호
+              </label>
+              <input
+                className="default-input"
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+                value={pwVal}
+                onChange={(e) => {
+                  onChangeHandler(e, "pw");
+                }}
+                onBlur={(e) => {
+                  onBlurHandler(e, "pw");
+                }}
+              />
+            </div>
+            {pwError ? (
+              <div className="form-error is-visible">
+                <div data-validation-attr={pwErrorMsg}>{pwErrorMsg}</div>
+              </div>
+            ) : null}
+          </div>
+          <div className={pwOkError ? "form-group error" : "form-group valid"}>
+            <div className="input-area">
+              <label className="blind" htmlFor="pw-reset-again">
+                비밀번호 재입력
+              </label>
+              <input
+                className="default-input"
+                type="password"
+                placeholder="비밀번호 확인"
+                value={pwOkVal}
+                onChange={(e) => {
+                  onChangeHandler(e, "pwOk");
+                }}
+                onBlur={(e) => {
+                  onBlurHandler(e, "pwOk");
+                }}
+              />
+            </div>
+            {pwOkError ? (
+              <div className="form-error is-visible">
+                <div data-validation-attr={pwOkErrorMsg}>{pwOkErrorMsg}</div>
+              </div>
+            ) : null}
+          </div>
+          <div className="bottom-fixed">
+            <button
+              type="button"
+              className="default-block-btn"
+              onClick={() => {
+                onClickHandler("pwChange");
+              }}
+            >
+              비밀번호 재설정
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
